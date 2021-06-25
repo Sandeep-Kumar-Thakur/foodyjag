c3122486216749f282630b1cc281bbf32e6a10ff
Your Name <77237189+Sandeep-Kumar-Thakur@users.noreply.github.com>
2021-06-25 11:53:02 +0530

    foodjag

---
+++ price.js
@@ -0,0 +1,49 @@
+window.addEventListener( 'load',() =>{
+    const iterm1 = "Rs.100"
+    const iterm2 = "Rs.150"
+    const iterm3 = "Rs.180"
+    const iterm4 = "Rs.210"
+    const iterm5 = "Rs.90"
+    const iterm6 = "Rs.400"
+    const iterm7 = "Rs.370"
+    const iterm8 = "Rs.88"
+    const iterm9 = "Rs.189"
+    const iterm10 = "Rs.110"
+    const iterm11 = "Rs.150"
+    const iterm12= "Rs.399"
+    const iterm13 = "Rs.124"
+    const iterm14 = "Rs.134"
+    const iterm15= "Rs.140"
+    const iterm16= "Rs.568"
+    const iterm17= "Rs.846"
+    const iterm18= "Rs.124"
+    const iterm19= "Rs.545"
+    const iterm20= "Rs.100"
+
+
+    
+    
+    document.getElementById('1').innerHTML = iterm1;
+    document.getElementById('2').innerHTML = iterm2;
+    document.getElementById('3').innerHTML = iterm3;
+    document.getElementById('4').innerHTML = iterm4;
+    document.getElementById('5').innerHTML = iterm5;
+    document.getElementById('6').innerHTML = iterm6;
+    document.getElementById('7').innerHTML = iterm7;
+    document.getElementById('8').innerHTML = iterm8;
+    document.getElementById('9').innerHTML = iterm9;
+    document.getElementById('10').innerHTML = iterm10;
+    document.getElementById('11').innerHTML = iterm11;
+    document.getElementById('12').innerHTML = iterm12;
+    document.getElementById('13').innerHTML = iterm13;
+    document.getElementById('14').innerHTML = iterm14;
+    document.getElementById('15').innerHTML = iterm15;
+    document.getElementById('16').innerHTML = iterm16;
+    document.getElementById('17').innerHTML = iterm17;
+    document.getElementById('18').innerHTML = iterm18;
+    document.getElementById('19').innerHTML = iterm19;
+    document.getElementById('20').innerHTML = iterm20;
+
+
+}
+)
